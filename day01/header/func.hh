e620993cc41f1eac51bad4563a75cd5051c9abf0
Guignet <guillaume.peugnet.2001@gmail.com>
2021-12-10 15:52:57 +0100

    adding directory for each day (with preimplement main/src/header/makefile)

---
+++ day01/header/func.hh
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+double print_number_increased(string const nomFich);
+
+double sum_vect(vector<double> to_sum);
+double count_calender_day1(string const nomFich);
+double count_calender_day1V2(string const nomFich);
