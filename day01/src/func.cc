d09df03be18d41b3c55dc2549c46a0238f1a62a1
Guignet <guillaume.peugnet.2001@gmail.com>
2021-12-10 16:39:47 +0100

    comment added

--- day01/src/func.cc
+++ day01/src/func.cc
@@ -1,44 +1,32 @@
 #include "../header/func.hh"
 
 double print_number_increased(string const nomFich){
-    // ofstream flux(nomFich.c_str());
-  ifstream floux(nomFich.c_str());
-  // if(flux){
-  //   flux << "YO idc " << endl;
-  //   flux << 26.42 << endl ;
-  //   int a = 42;
-  //   flux << "OMG y'a "<< a << endl;
-  // }
-  // else{
-  //   cout << "ERROR: could not open file" << endl;
-  // }
-  if(floux){
+//function to get the number of increase value from previous to current value 
+  ifstream floux(nomFich.c_str()); // we open the file
+  if(floux){    //we verify that the files exist and is open
     double test;
     double previous = -1;
     double count_increased = 0;
-    while(floux >> test){
-      // cout << test << endl;
-      if(previous != -1){
-        if(test>previous){
-          count_increased++;
+    while(floux >> test){   // we put a number from floux to test
+      if(previous != -1){   //first init case
+        if(test>previous){  // check the difference
+          count_increased++;    //increase the count 
         }
-
-        
-
       }
-      previous = test;
+      previous = test;  // previous become current(test)
     }
-    cout <<"number increased : " << count_increased << endl;
+    cout <<"number increased : " << count_increased << endl; //we print the final number before returning it 
     return count_increased;
   }
   else{
-    cout << "ERROR: could not read the file" << endl;
-      return -1;
+    cout << "ERROR: could not read the file" << endl;   //in case the file does not exist
+      return -1;    //error return
   }
 
 }
 
 double sum_vect(vector<double> to_sum){
+    //to make the sum of each element of a vector
     double sum = 0;
     for(double a: to_sum){
         sum+=a;
@@ -46,162 +34,65 @@ double sum_vect(vector<double> to_sum){
     return sum;
 }
 
-int increased_counter(double previous, double current){
-    if(current > previous)
-    return 1;
-    return 0;
-}
 
 double count_calender_day1(string const nomFich){
-    ifstream floux(nomFich.c_str());
-    ofstream flix("src/tmp.txt");
-    if(flix)
-        if(floux){
-            vector<double> A;
-            vector<double> B;
-            vector<double> C;
-            vector<double> D;
-            double inp;
-            double previous = 0;
-            double current = 0;
-            double count_increased = 0;
-            int count_read;
-            while(floux>>inp){
-                switch (count_read % 4){
-                    case 0:
-                        /* code */
-                        A.push_back(inp);
-                        if(count_read>2){
-                            C.push_back(inp);
-                            D.push_back(inp);
-                            current = sum_vect(C);
-                            C.resize(0);
-                            count_increased += increased_counter(previous,current);
-                            previous = current;
-                        }
-
-                        count_read++;
-                    break;
-                    case 1:
-                        A.push_back(inp);
-                        B.push_back(inp);
-                        if(count_read>2){
-                            D.push_back(inp);
-                            
-                            current = sum_vect(D);
-                            D.resize(0);
-                            count_increased += increased_counter(previous,current);
-
-                            previous = current;
-                        }
-                        count_read++;
-                    break;
-                    case 2:
-                        A.push_back(inp);
-                        B.push_back(inp);
-                        C.push_back(inp);
-                        current = sum_vect(A);
-                        A.resize(0);
-                        if(previous!=0)
-                        count_increased += increased_counter(previous,current);
-                        previous = current;
-                        
-                        count_read++;
-                    break;
-                    case 3:
-                        B.push_back(inp);
-                        C.push_back(inp);
-                        D.push_back(inp);
-                        
-                        current = sum_vect(B);
-                        D.resize(0);
-                        count_increased += increased_counter(previous,current);
-                        previous = current;
-                        
-                        count_read++;
-                    break;
-                }
-            }
-
-            return count_increased;
-        }
-        else{
-            cout << "ERROR: Could not read files" << endl;
-            return -1;
-        }
-        else{
-        cout << "ERROR: Could not read files" << endl;
-        return -1;
-
-        }
-
-}
-
-double count_calender_day1V2(string const nomFich){
-    ifstream floux(nomFich.c_str());
-    ofstream flix("src/tmp.txt");
+    //second part of the day
+    ifstream floux(nomFich.c_str());    //read files
+    ofstream flix("src/tmp.txt");       //write files 
     
-    if(floux){
+    if(floux){  // check if it exist
+        //init of vector 
         vector<double> A;
         vector<double> B;
         vector<double> C;
         vector<double> D;
-        double inp;
-        int count_read = 0;
+        double inp; // input
+        int count_read = 0; //count of elements read
+        
         while(floux>>inp){
-            switch (count_read % 4){
+            switch (count_read % 4){    //we divide the code in 4 cases
                 case 0:
-                    /* code */
-                    A.push_back(inp);
-                    if(count_read>2){
-                        C.push_back(inp);
+                    A.push_back(inp);   //we push number read in A
+                    if(count_read>2){   // for the init
+                        C.push_back(inp);   
                         D.push_back(inp);
-                        flix << sum_vect(C)<<endl;
-                        C.resize(0);
+                        flix << sum_vect(C)<<endl;  //in this case C will be full so we make the sum of each elements of C and write it in a file
+                        C.resize(0);    //we empty C 
                     }
                     count_read++;
                 break;
-                case 1:
+                case 1: //as case 0
                     A.push_back(inp);
                     B.push_back(inp);
                     if(count_read>2){
-                        D.push_back(inp);
-                        
+                        D.push_back(inp);                       
                         flix << sum_vect(D)<<endl;
                         D.resize(0);
-
                     }
                     count_read++;
                 break;
-                case 2:
+                case 2: //as case 0 whithout the init
                     A.push_back(inp);
                     B.push_back(inp);
                     C.push_back(inp);
                     flix <<  sum_vect(A)<<endl;
                     A.resize(0);
-                    
                     count_read++;
                 break;
-                case 3:
+                case 3: //as case 0 whithout the init
                     B.push_back(inp);
                     C.push_back(inp);
                     D.push_back(inp);
-                    
                     flix << sum_vect(B)<<endl;
                     B.resize(0);
-                    
                     count_read++;
                 break;
             }
         }
-
-        return print_number_increased("src/tmp.txt");
+        return print_number_increased("src/tmp.txt");   //we use the previous programm with the new file created
     }
     else{
         cout << "ERROR: Could not read files" << endl;
         return -1;
     }
-    
-    
-
 }
