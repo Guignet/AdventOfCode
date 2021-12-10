de4f6a45c6d4831b21058a7df2a37c132c584e90
Guignet <guillaume.peugnet.2001@gmail.com>
2021-12-10 15:18:37 +0100

    first day dir and second day dir

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # AdventOfCode
 my repo of the advent of code
+
+## To DO 
+fill up the Readme.md to explain the code 
+
+# Day 1 
+```bash
+make
+```
+to run the proramm
+
+# Day 2
---
+++ day2/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "header/func.hh"
+
+using namespace std;
+int main(){
+  cout << "number increased 3 in a row :" << count_calender_day1V2("src/keep.txt") 
+  << endl;
+  return 0;
+}
